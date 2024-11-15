07564efd1434f0791a6ebf418924392e146968c2
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-11-15 17:03:34 +0000

    Completed assignment 3

---
+++ Week 3/function.py
@@ -0,0 +1,14 @@
+## Question 1
+def calculate_discount(price, discount_percent):
+    discount = price * (discount_percent / 100)
+    
+    if discount_percent >= 20:
+        return price - discount
+    return price
+
+
+## Question 2
+original_price = float(input("Enter the price: "))
+discount_percentage = float(input("Enter the discount percent: "))
+result = float(calculate_discount(original_price, discount_percentage))
+print(result) 
